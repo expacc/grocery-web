551e3559dc8088fa137a83ac36b0b4c526775c73
Hanjara CA (HANS) <mangatkk@gmail.com>
2020-06-13 17:32:25 +0700

    Hide Strype and Paypal

--- resources/views/settings/app/payment.blade.php
+++ resources/views/settings/app/payment.blade.php
@@ -25,9 +25,12 @@
         <div class="card-body">
             {!! Form::open(['url' => ['settings/update'], 'method' => 'patch']) !!}
             <div class="row">
-                <h5 class="col-12 pb-4"><i class="mr-3 fa fa-cc-stripe"></i>{!! trans('lang.app_setting_stripe') !!}</h5>
+                <!------------------------------------------------------------------
+                - STRIPE PAYMENT
+                ------------------------------------------------------------------->
+                <!-- <h5 class="col-12 pb-4"><i class="mr-3 fa fa-cc-stripe"></i>{!! trans('lang.app_setting_stripe') !!}</h5> -->
                 <!-- 'Boolean enable_facebook Field' -->
-                <div class="form-group row col-12">
+                <!-- <div class="form-group row col-12">
                     {!! Form::label('enable_stripe', trans('lang.app_setting_enable_stripe'),['class' => 'col-2 control-label text-right']) !!}
                     <div class="checkbox icheck">
                         <label class="w-100 ml-2 form-check-inline">
@@ -36,9 +39,9 @@
                             <span class="ml-2">{!! trans('lang.app_setting_enable_stripe_help') !!}</span>
                         </label>
                     </div>
-                </div>
+                </div> -->
                 <!-- facebook_app_id Field -->
-                <div class="form-group row col-6">
+                <!-- <div class="form-group row col-6">
                     {!! Form::label('stripe_key', trans('lang.app_setting_stripe_key'), ['class' => 'col-4 control-label text-right']) !!}
                     <div class="col-8">
                         {!! Form::text('stripe_key', setting('stripe_key'),  ['class' => 'form-control','placeholder'=>  trans('lang.app_setting_stripe_key_placeholder')]) !!}
@@ -46,10 +49,9 @@
                             {!! trans('lang.app_setting_stripe_key_help') !!}
                         </div>
                     </div>
-                </div>
-
+                </div> -->
                 <!-- facebook_app_secret Field -->
-                <div class="form-group row col-6">
+                <!-- <div class="form-group row col-6">
                     {!! Form::label('stripe_secret', trans('lang.app_setting_stripe_secret'), ['class' => 'col-4 control-label text-right']) !!}
                     <div class="col-8">
                         {!! Form::text('stripe_secret', setting('stripe_secret'),  ['class' => 'form-control','placeholder'=>  trans('lang.app_setting_stripe_secret_placeholder')]) !!}
@@ -57,11 +59,14 @@
                             {!! trans('lang.app_setting_stripe_secret_help') !!}
                         </div>
                     </div>
-                </div>
+                </div> -->
 
-                <h5 class="col-12 pb-4 custom-field-container"><i class="mr-3 fa fa-cc-paypal"></i>{!! trans('lang.app_setting_paypal') !!}</h5>
+                <!------------------------------------------------------------------
+                - PAYPAL PAYMENT
+                ------------------------------------------------------------------->
+                <!-- <h5 class="col-12 pb-4 custom-field-container"><i class="mr-3 fa fa-cc-paypal"></i>{!! trans('lang.app_setting_paypal') !!}</h5> -->
                 <!-- 'Boolean enable_facebook Field' -->
-                <div class="form-group row col-12">
+                <!-- <div class="form-group row col-12">
                     {!! Form::label('enable_paypal', trans('lang.app_setting_enable_paypal'),['class' => 'col-2 control-label text-right']) !!}
                     <div class="checkbox icheck">
                         <label class="w-100 ml-2 form-check-inline">
@@ -70,10 +75,9 @@
                             <span class="ml-2">{!! trans('lang.app_setting_enable_paypal_help') !!}</span>
                         </label>
                     </div>
-                </div>
-
+                </div> -->
                 <!-- 'Boolean enable_facebook Field' -->
-                <div class="form-group row col-12">
+                <!-- <div class="form-group row col-12">
                     {!! Form::label('paypal_mode', trans('lang.app_setting_paypal_mode'),['class' => 'col-2 control-label text-right']) !!}
                     <div class="checkbox icheck">
                         <label class="w-100 ml-2 form-check-inline">
@@ -82,10 +86,9 @@
                             <span class="ml-2">{!! trans('lang.app_setting_paypal_mode_help') !!}</span>
                         </label>
                     </div>
-                </div>
-                
+                </div> -->
                 <!-- paypal_username Field -->
-                <div class="form-group row col-6">
+                <!-- <div class="form-group row col-6">
                     {!! Form::label('paypal_username', trans('lang.app_setting_paypal_username'), ['class' => 'col-4 control-label text-right']) !!}
                     <div class="col-8">
                         {!! Form::text('paypal_username', setting('paypal_username'),  ['class' => 'form-control','placeholder'=>  trans('lang.app_setting_paypal_username_placeholder')]) !!}
@@ -93,10 +96,9 @@
                             {!! trans('lang.app_setting_paypal_username_help') !!}
                         </div>
                     </div>
-                </div>
-
+                </div> -->
                 <!-- paypal_password Field -->
-                <div class="form-group row col-6">
+                <!-- <div class="form-group row col-6">
                     {!! Form::label('paypal_password', trans('lang.app_setting_paypal_password'), ['class' => 'col-4 control-label text-right']) !!}
                     <div class="col-8">
                         {!! Form::text('paypal_password', setting('paypal_password'),  ['class' => 'form-control','placeholder'=>  trans('lang.app_setting_paypal_password_placeholder')]) !!}
@@ -104,11 +106,9 @@
                             {!! trans('lang.app_setting_paypal_password_help') !!}
                         </div>
                     </div>
-                </div>
-
-
+                </div> -->
                 <!-- facebook_app_secret Field -->
-                <div class="form-group row col-6">
+                <!-- <div class="form-group row col-6">
                     {!! Form::label('paypal_secret', trans('lang.app_setting_paypal_secret'), ['class' => 'col-4 control-label text-right']) !!}
                     <div class="col-8">
                         {!! Form::text('paypal_secret', setting('paypal_secret'),  ['class' => 'form-control','placeholder'=>  trans('lang.app_setting_paypal_secret_placeholder')]) !!}
@@ -116,10 +116,9 @@
                             {!! trans('lang.app_setting_paypal_secret_help') !!}
                         </div>
                     </div>
-                </div>
-
+                </div> -->
                 <!-- paypal_app_app_id Field -->
-                <div class="form-group row col-6">
+                <!-- <div class="form-group row col-6">
                     {!! Form::label('paypal_app_id', trans('lang.app_setting_paypal_app_id'), ['class' => 'col-4 control-label text-right']) !!}
                     <div class="col-8">
                         {!! Form::text('paypal_app_id', setting('paypal_app_id'),  ['class' => 'form-control','placeholder'=>  trans('lang.app_setting_paypal_app_id_placeholder')]) !!}
@@ -127,8 +126,12 @@
                             {!! trans('lang.app_setting_paypal_app_id_help') !!}
                         </div>
                     </div>
-                </div>
+                </div> -->
 
+
+                <!------------------------------------------------------------------
+                - TAX PAYMENT
+                ------------------------------------------------------------------->
                 <h5 class="col-12 pb-4 custom-field-container"><i class="mr-3 fa fa-money"></i>{!! trans('lang.app_setting_default_tax') !!}</h5>
                 <!-- default_tax Field -->
                 <div class="form-group row col-6">
@@ -141,6 +144,9 @@
                     </div>
                 </div>
 
+                <!------------------------------------------------------------------
+                - CURRENCY PAYMENT
+                ------------------------------------------------------------------->
                 <h5 class="col-12 pb-4 custom-field-container"><i class="mr-3 fa fa-money"></i>{!! trans('lang.app_setting_default_currency') !!}</h5>
                 <!-- default_currency Field -->
                 <div class="form-group row col-6">
